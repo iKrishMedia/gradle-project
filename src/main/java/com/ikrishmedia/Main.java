5cafb3854ef0989a663797a639e7b89d05d9a2b6
iKrishMedia <ikrishmedia@gmail.com>
2025-07-23 22:13:45 -0400

    commit+push 1

--- src/main/java/com/ikrishmedia/Main.java
+++ src/main/java/com/ikrishmedia/Main.java
@@ -5,6 +5,8 @@ package com.ikrishmedia;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World 1");
+        System.out.println("Hello, World 2");
+        System.out.println("Hello, World 3");
     }
 }
